551fe2f2f84c5b6972197d45de23d2d836148612
Olga <akulshina.o.a@gmail.com>
2024-01-12 23:06:41 +0100

    dz_5.8

---
+++ 5-languages/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+switch (lang){
+    case 'en':
+        console.log('Hello!');
+        break;
+    case 'es':
+        console.log('Hola!');
+        break;
+    case 'ru':
+        console.log('Здравствуйте!');
+        break;
+    case 'fr':
+        console.log('Bonjour!');
+        break;
+    case 'de':
+        console.log('Gutten tag!');
+        break;
+    default:
+        console.log('Выберите другой язык!')
+}
