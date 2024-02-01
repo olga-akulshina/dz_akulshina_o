c5801e62d3e010e8753b67da73cc1c802cf887dd
Olga <akulshina.o.a@gmail.com>
2024-02-01 16:10:56 +0100

    dz_11.13

---
+++ 11-date-array/script.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const arr = ['10-02-2022', 'тест', '11/12/2023', '00/13/2022', '41/12/2023'];
+
+function selectDates(array){
+    return array
+        .filter(el => { return Date.parse(el)})
+        .map(el => {
+            if(el.includes('/')){
+                let splitted = el.split('/');
+                let tmp = [];
+                tmp.push(splitted[1]);
+                tmp.push(splitted[0]);
+                tmp.push(splitted[2]);
+                return tmp.join('-');
+            } else {
+                return el;
+            }
+    })
+}
+
+
+console.log(selectDates(arr));
+
+
+
+
